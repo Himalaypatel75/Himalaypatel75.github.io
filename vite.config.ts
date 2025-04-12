55051c04b0cc2b4f32cacfb2fcf34a8621fb2715
himalay patel <himalaypatel75@gmail.com>
2025-04-12 11:55:02 +0530

    updating some files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Himalaypatel75.github.io/',
   server: {
     host: "::",
     port: 8080,
